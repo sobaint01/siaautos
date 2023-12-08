07ddc50cf3ae4b6e0170d30ba93910bde5746fb2
sobaint01 <walebellomail@gmail.com>
2023-12-08 12:53:34 -0500

    java

--- js/index.js
+++ js/index.js
@@ -1,22 +1,29 @@
-let x = 11
-let y = 25
+// let x = 11
+// let y = 25
 
-let total = x+y
+// let total = x+y
 
-console.log(total)
+// console.log(total)
 
 let openHamburger = document.getElementById("hamburg")
 let closeHamburger = document.getElementById("hamburgCloser")
+let Menu = document.getElementById("dropMenu")
 
 openHamburger.addEventListener("click", 
 function() {
     closeHamburger.style.display = "flex";
     openHamburger.style.display = "none";
+    Menu.style.display = "block";
+    document.getElementsByClassName("primaryNav").style.marginTop = "auto";
+    
         
 })
 
 closeHamburger.addEventListener("click", 
 function() {
-    openHamburger.style.display = "flex";
-    closeHamburger.style.display = "none";  
+    openHamburger.style.display = "block";
+    closeHamburger.style.display = "none"; 
+    Menu.style.display = "none";
+    // document.getElementsByClassName("primaryNav").style.marginTop = "auto";
+
 })
